2108815ff0d3d4c51503e0a858ca1ca9a4635751
Kuramura <chloeburgess21@yahoo.co.uk>
2019-10-21 16:57:50 +0100

    Added Unity Files

---
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rigidbodyComponent;
+
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rigidbodyComponent.velocity = new Vector2(x, y) * speed;
+        rigidbodyComponent.angularVelocity = 0.0f;
+    }
+}
